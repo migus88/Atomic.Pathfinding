25aa0c25e571a700dfd38d1751801afa10563d8f
Yuri Sokolov <7556847+migus88@users.noreply.github.com>
2024-11-28 22:46:40 +0200

    reduced allocations in demo

--- atomic-pathfinding-unity/Assets/Demo/Code/Player.cs
+++ atomic-pathfinding-unity/Assets/Demo/Code/Player.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using Atomic.Pathfinding.Core.Data;
+using Atomic.Pathfinding.Core.Helpers;
 using Atomic.Pathfinding.Core.Interfaces;
 using UnityEngine;
 
@@ -16,13 +17,19 @@ namespace Demo
         [SerializeField, Range(1,2)] private int _size = 1;
         
         private bool _isMoving;
+        
+        // This is a hacky way to reduce allocations.
+        // Instead of allocating a new collection, we're using pointers,
+        // and in order to use asynchronous code with pointers, we cast it to IntPtr.
+        private IntPtr _path;
+        private int _pathLength;
 
         private void Start()
         {
             Battlefield.Instance.CellClicked += OnCellClicked;
         }
 
-        private void OnCellClicked(Cell cell)
+        private unsafe void OnCellClicked(Cell cell)
         {
             if (_isMoving)
             {
@@ -34,20 +41,23 @@ namespace Demo
             
             if(result.IsPathFound)
             {
-                StartCoroutine(MoveToPoint(result.Path));
+                _path = (IntPtr)result.Path.ToCoordinateArrayPointer();
+                _pathLength = result.Path.Length;
+                StartCoroutine(MoveToPoint());
             }
         }
         
         
-        private IEnumerator MoveToPoint(Coordinate[] path)
+        private IEnumerator MoveToPoint()
         {
             _isMoving = true;
-            
-            foreach (var coordinate in path)
+
+            for (var i = 0; i < _pathLength; i++)
             {
+                var coordinate = GetCoordinate(i);
                 var cell = Battlefield.Instance.GetFieldCell(coordinate.X, coordinate.Y);
                 var waypoint = cell.transform.position;
-                
+
                 while (Vector3.Distance(transform.position, waypoint) > 0.01f)
                 {
                     transform.position = Vector3.Lerp(transform.position, waypoint, Speed * Time.deltaTime);
@@ -58,9 +68,11 @@ namespace Demo
                 transform.position = waypoint;
                 Coordinate = cell.Cell.Coordinate;
             }
-            
+
             _isMoving = false;
         }
+        
+        private unsafe Coordinate GetCoordinate(int index) => ((Coordinate*)_path)![index];
 
         private void OnDestroy()
         {
